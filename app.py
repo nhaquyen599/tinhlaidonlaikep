02bc58b922aa7705799a1e8f8c17e84db93fa86b
binhquytoc-lab <binhquytoc@gmail.com>
2026-06-16 12:05:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.image("logo.jpg")
 # Tiêu đề app
-st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM NGÂN HÀNG")
+st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM NGÂN HÀNG_TS. VŨ ĐỨC BÌNH")
 
 # Nhập dữ liệu
 C = st.number_input(
